2243c5bb50418e4335b2e59cc04257a6f0af7b28
Eason Nie <nyixin318@gmail.com>
2016-07-21 23:37:18 -0500

    skip-product

--- model/snli_skipth_model.py
+++ model/snli_skipth_model.py
@@ -13,10 +13,11 @@ class BasicSkipThought:
         self.input_loader = SnliSkipThoughtLoader(sentence_d=sentence_d)
         self.sentence_embedding_output = tf.concat(1, [self.input_loader.premise, self.input_loader.hypothesis,
                                                        tf.abs(
-                                                           self.input_loader.premise - self.input_loader.hypothesis)])
+                                                           self.input_loader.premise - self.input_loader.hypothesis),
+                                                       tf.mul(self.input_loader.premise, self.input_loader.hypothesis)])
 
         self.output = nn_layer(tf.nn.dropout(self.sentence_embedding_output, keep_prob=softmax_keeprate),
-                               shape=[sentence_d * 3, num_class],
+                               shape=[sentence_d * 4, num_class],
                                name='affine-softmax',
                                w_init=tf.contrib.layers.xavier_initializer(uniform=False),
                                b_init=tf.constant_initializer(0.0, dtype=tf.float32),
