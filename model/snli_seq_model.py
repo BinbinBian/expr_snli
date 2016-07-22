54c53bb0b85ef7579fa75ef31e8e8d5492a9e515
Eason Nie <nyixin318@gmail.com>
2016-07-21 22:36:00 -0500

    Push for biLSTM model.

--- model/snli_seq_model.py
+++ model/snli_seq_model.py
@@ -113,3 +113,87 @@ class SnliBasicLSTM:
         self.setup()
         d_pred = self.sess.run(self.prediction, feed_dict=feed_dict)
         print(d_pred)
+
+
+class biLSTM(SnliBasicLSTM):
+    def __init__(self, lstm_step=80, input_d=300, vocab_size=2196018, hidden_d=100, num_class=3, learning_rate=0.001,
+                 softmax_keeprate=0.75, lstm_input_keep_rate=1.0, lstm_output_keep_rate=0.9, embedding=None, **kwargs):
+        self.model_info = record_info(LSTM_Step=lstm_step,
+                                      Word_Dimension=input_d,
+                                      Vocabluary_Size=vocab_size,
+                                      LSTM_Hidden_Dimension=hidden_d,
+                                      Number_Class=num_class,
+                                      SoftMax_Keep_Rate=softmax_keeprate,
+                                      LSTM_Input_Keep_Rate=lstm_input_keep_rate,
+                                      LSTM_Output_Keep_Rate=lstm_output_keep_rate,
+                                      kwargs=kwargs)
+
+        self.input_loader = SnliLoader(lstm_step, input_d, vocab_size, embedding)
+
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_d, state_is_tuple=True)
+        lstm_cell_r = tf.nn.rnn_cell.BasicLSTMCell(hidden_d, state_is_tuple=True)
+
+        basic_seq_lstm_premise = BasicSeqModel(input_=self.input_loader.premise,
+                                               length_=self.input_loader.premise_length,
+                                               hidden_state_d=hidden_d,
+                                               name='premise-lstm', cell=[lstm_cell, lstm_cell_r],
+                                               input_keep_rate=lstm_input_keep_rate,
+                                               output_keep_rate=lstm_output_keep_rate)
+
+        basic_seq_lstm_hypothesis = BasicSeqModel(input_=self.input_loader.hypothesis,
+                                                  length_=self.input_loader.hypothesis_length,
+                                                  hidden_state_d=hidden_d,
+                                                  name='hypothesis-lstm', cell=[lstm_cell, lstm_cell_r],
+                                                  input_keep_rate=lstm_input_keep_rate,
+                                                  output_keep_rate=lstm_output_keep_rate)
+
+        self.premise_lstm_last = basic_seq_lstm_premise.last
+        self.r_premise_lstm_last = basic_seq_lstm_premise.reverse_last
+
+        self.hypothesis_lstm_last = basic_seq_lstm_hypothesis.last
+        self.r_hypothesis_lstm_last = basic_seq_lstm_hypothesis.reverse_last
+
+        self.premise_embedding = tf.concat(1, [self.premise_lstm_last, self.r_premise_lstm_last])
+        self.hypothesis_embedding = tf.concat(1, [self.hypothesis_lstm_last, self.r_hypothesis_lstm_last])
+
+        self.sentence_embedding_output = tf.concat(1, [self.premise_embedding, self.hypothesis_embedding,
+                                                       tf.abs(self.premise_embedding - self.hypothesis_embedding),
+                                                       tf.mul(self.premise_embedding, self.hypothesis_embedding)])
+
+        layer_1_output = nn_layer(self.sentence_embedding_output,
+                                  shape=[hidden_d * 8, hidden_d * 8],
+                                  name='layer-1',
+                                  w_init=tf.contrib.layers.xavier_initializer(uniform=True),
+                                  b_init=tf.constant_initializer(0.0, dtype=tf.float32),
+                                  act=tf.nn.tanh)
+
+        layer_2_output = nn_layer(layer_1_output,
+                                  shape=[hidden_d * 8, hidden_d * 8],
+                                  name='layer-2',
+                                  w_init=tf.contrib.layers.xavier_initializer(uniform=True),
+                                  b_init=tf.constant_initializer(0.0, dtype=tf.float32),
+                                  act=tf.nn.tanh)
+
+        layer_3_output = nn_layer(layer_2_output,
+                                  shape=[hidden_d * 8, hidden_d * 8],
+                                  name='layer-3',
+                                  w_init=tf.contrib.layers.xavier_initializer(uniform=True),
+                                  b_init=tf.constant_initializer(0.0, dtype=tf.float32),
+                                  act=tf.nn.tanh,
+                                  output_keep=softmax_keeprate)
+
+        self.output = nn_layer(layer_3_output,
+                               shape=[hidden_d * 8, num_class],
+                               name='softmax-affine-layer',
+                               w_init=tf.contrib.layers.xavier_initializer(uniform=False),
+                               b_init=tf.constant_initializer(0.0, dtype=tf.float32),
+                               act=None)
+
+        self.softmax_output = tf.nn.softmax(self.output)
+        self.prediction = tf.argmax(self.softmax_output, dimension=1)
+
+        self.cost = tf.nn.sparse_softmax_cross_entropy_with_logits(self.output, self.input_loader.label)
+
+        self.train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
+        self.init_op = tf.initialize_all_variables()
+        self.sess = tf.Session()
