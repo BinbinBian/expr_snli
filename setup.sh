1d65c05ead78ae1d89ff3dc68bc41a43a0b29e14
Eason Nie <nyixin318@gmail.com>
2016-07-23 17:31:27 -0500

    Fix small char.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 export CUDA_HOME=$SHARED_ROOT/cuda-7.5
 export LD_LIBRARY_PATH=$SHARED_ROOT/cuda-7.5/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$SHARED_ROOT/cudnn-4.0.7/lib64:$LD_LIBRARY_PATH
